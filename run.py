c43918b95fe0ade39c293ee86b2924f2df7e2706
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2023-01-28 20:32:26 -0500

    code inject done \o/

--- run.py
+++ run.py
@@ -3,9 +3,15 @@ import os
 import sys
 import subprocess
 import traceback
+import shutil
+import glob
+
+def print_and_run(*cmd):
+  print('> {}'.format(' '.join(list(cmd))))
+  subprocess.run(list(cmd), check=True)
 
 def main(args=sys.argv):
-  if len(args) > 0 and 'python' in args[0]:
+  while len(args) > 0 and ('python' in args[0] or os.path.basename(__file__) in args[0]):
     args = args[1:]
 
   tsv_dir = None
@@ -23,11 +29,83 @@ def main(args=sys.argv):
     print('Unknown TSV directory {}, exiting...'.format(tsv_dir))
     return
 
-  # Open .jar file, scan for packaged java, run app
+  print('TSV directory is: {}'.format(tsv_dir))
 
-  subprocess.run([
-    f'', ''
-  ])
+  tsv_path_items = [
+    os.path.join(tsv_dir, 'jre', 'bin'),
+    os.path.join(tsv_dir, 'jnilib'),
+    os.path.join(tsv_dir, 'jnilib', 'vtk'),
+    os.path.join(tsv_dir, 'dist', 'glpk-4.55'),
+  ]
+  os.environ['PATH'] = os.pathsep.join(tsv_path_items)+os.pathsep+os.environ['PATH']
+
+  # Open .jar file, scan for packaged java, run app
+  java_exe = None
+  if shutil.which('java'):
+    java_exe = shutil.which('java')
+
+  if java_exe is None:
+    java_exe = os.path.join(tsv_dir, 'jre', 'bin', 'java.exe')
+
+  print('Java is: {}'.format(java_exe))
+
+  if 'de' in args:
+    if not os.path.exists('dist/jd-gui-1.6.6.jar'):
+      print('Ensure dist/jd-gui-1.6.6.jar exists!')
+      return
+
+    print_and_run(
+      java_exe, '-jar', os.path.join('dist', 'jd-gui-1.6.6.jar'), os.path.join(tsv_dir, 'dist', 'atsv.jar')
+    )
+    return
+
+
+  # We try to compile the plugin if we have javac, else we
+  # assume the plugin's .class files are all under the folder dist/*.class
+  javac_exe = None
+  if shutil.which('javac'):
+    javac_exe = shutil.which('javac')
+
+  tsv_map_plusin_dist = os.path.join(os.path.dirname(__file__), 'dist')
+
+  if not os.path.exists(tsv_map_plusin_dist):
+    os.makedirs(tsv_map_plusin_dist)
+
+  if javac_exe is None:
+    print()
+    print('Cannot find javac, assuming plugin is already built under dist/')
+    print('If map graph does not appear check the dist/ directory.')
+    print()
+    time.sleep(2)
+  else:
+    # perform build
+    src_java_files = [
+      x for x in glob.glob('src/*.java', recursive=True)
+    ]
+    tsv_classpath = [
+      os.path.join(tsv_dir, 'dist', 'atsv.jar'),
+      os.path.join(tsv_dir, 'dist', 'parser.jar'),
+    ]
+    if len(src_java_files) > 0:
+      print_and_run(
+        javac_exe,
+          '-d', tsv_map_plusin_dist,
+          '-cp', os.pathsep.join(tsv_classpath),
+          *src_java_files
+      )
+
+  # Add our dist/ to the plugin class path
+  tsv_classpath.append(
+    tsv_map_plusin_dist
+  )
+
+  print_and_run(
+    java_exe,
+    '-cp', os.pathsep.join(tsv_classpath),
+    '-Xms4G', '-Xmx8G', '-Dsun.java2d.ddoffscreen=true', '-Dsun.java2d.gdiblit=true',
+    #'DataVis' # Name of TSV class with main() defined
+    'DataVisWithMap' # Our copy of DataVis
+  )
 
 
 if __name__ == '__main__':
---
+++ src/DataVisWithMap.java
@@ -0,0 +1,56 @@
+import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
+import java.io.File;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import nddv.data.DatasetLoader;
+import nddv.gui.ATSVFrontend;
+import nddv.gui.ATSVNoSplashScreenNoCloseOperation;
+import nddv.gui.AbstractDetailWindow;
+import nddv.gui.DetailWindow;
+import nddv.gui.mainframe.ATSVMainFrameTemplate;
+import nddv.gui.mainframe.basicdisplay.ATSVBasicMainFrameTemplate;
+import nddv.validator.ObjectCreator;
+
+public class DataVisWithMap {
+  public static void main(String[] args) {
+    
+    System.out.println("in DataVisWithMap!");
+    try { Thread.sleep(5000); } catch (Exception e) { }
+
+    try {
+      UIManager.setLookAndFeel(new WindowsLookAndFeel());
+    } catch (UnsupportedLookAndFeelException e) {
+      System.out.println("Does Not Support Windows Look and Feel");
+    } catch (Exception e) {
+      e.printStackTrace();
+    } 
+    new ObjectCreator();
+    if (args.length == 0) {
+      new ATSVFrontend();
+    } else if (args.length == 1 && args[0].equals("mainFrame")) {
+      ATSVBasicMainFrameTemplate mainframe = new ATSVBasicMainFrameTemplate(3);
+      mainframe.getMainFrame().setDetailWindow((AbstractDetailWindow)new DetailWindow());
+      try {
+        mainframe.getMainFrame().loadNewDesignSpace(
+            DatasetLoader.load(new File("../data/carDatabase/CarData-updated.csv"), 10));
+      } catch (Exception e) {
+        try {
+          mainframe.getMainFrame().loadNewDesignSpace(
+              DatasetLoader.load(new File("./data/carDatabase/CarData-updated.csv"), 10));
+        } catch (Exception f) {
+          f.printStackTrace();
+        } 
+      } 
+    } else if (args.length == 1 && args[0].equals("noSplashNoSystemExit")) {
+      new ATSVNoSplashScreenNoCloseOperation();
+    } else if (args.length == 1 && args[0].equals("mainFrameSystemExitonClose")) {
+      new ATSVMainFrameTemplate(1);
+    } else if (args.length == 1) {
+      new ATSVFrontend(args[0]);
+    } else if (args.length == 2 && args[0].equals("engine")) {
+      new ATSVFrontend(args[1], true);
+    } else {
+      new ATSVFrontend();
+    } 
+  }
+}
