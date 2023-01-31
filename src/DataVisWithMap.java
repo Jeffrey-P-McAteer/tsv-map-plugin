632809a0d90ce4fda8d8ceba1200f9b329367070
McAteer, Jeffrey P CIV STRATCOM JWAC (US) <jeffrey.p.mcateer.civ@mail.mil>
2023-01-31 08:38:21 -0500

    Menu UI injection

--- src/DataVisWithMap.java
+++ src/DataVisWithMap.java
@@ -60,12 +60,17 @@ public class DataVisWithMap {
       try { Thread.sleep(5000); } catch (Exception e) { } // wait 5s for UI to initialize
       javax.swing.SwingUtilities.invokeLater(() -> {
         try {
-          DataVisWithMap.<java.util.Vector<nddv.data.menu.ATSVMenuControls>>reflect_get(ui_frontend, "menuMiscControls").add(new nddv.data.menu.ATSVMenuControls(){
-            { // Anon initializer
-              this.menuItem = new javax.swing.JMenuItem("Some nonsense");
-            }
-          });
-          System.out.println("UI modified!");
+          // DataVisWithMap.<java.util.Vector<nddv.data.menu.ATSVMenuControls>>reflect_get(ui_frontend, "menuMiscControls").add(new nddv.data.menu.ATSVMenuControls(){
+          //   { // Anon initializer
+          //     this.menuItem = new javax.swing.JMenuItem("Some nonsense");
+          //   }
+          // });
+          // DataVisWithMap.<Void>reflect_call(ui_frontend, "activateMenuItems", true); // reads menuMiscControls & creates UI
+          // System.out.println("UI modified!");
+          javax.swing.JMenuBar app_menu_bar = ui_frontend.getJMenuBar();
+          ((javax.swing.JMenu) app_menu_bar.getSubElements()[4]).add(
+            new javax.swing.JMenuItem("Some nonsense")
+          );
 
         }
         catch (Exception e) {
@@ -95,6 +100,52 @@ public class DataVisWithMap {
     }
     catch (Exception e) {
       System.out.println(e);
+    }
+    return null;
+  }
+
+  public static <T> T reflect_call(Object o, String method_name, Object... args) { // statics
+    Class<?>[] arg_classes = new Class<?>[args.length];
+    try {
+      for (int i=0; i<args.length; i+=1) {
+        arg_classes[i] = args[i].getClass();
+      }
+      Method m = o.getClass().getDeclaredMethod(method_name, arg_classes);
+      m.setAccessible(true);
+      return (T) m.invoke(o, args);
+    }
+    catch (Exception e) {
+      if (e instanceof java.lang.NoSuchMethodException) {
+        // If any of arg_classes is Boolean, Integer, Double, down-cast to primitive and try again.
+        boolean down_casted_an_arg = false;
+        for (int i=0; i<args.length; i+=1) {
+          if (arg_classes[i] == Boolean.class) {
+            arg_classes[i] = boolean.class;
+            down_casted_an_arg = true;
+          }
+          else if (arg_classes[i] == Integer.class) {
+            arg_classes[i] = int.class;
+            down_casted_an_arg = true;
+          }
+          else if (arg_classes[i] == Double.class) {
+            arg_classes[i] = double.class;
+            down_casted_an_arg = true;
+          }
+        }
+        if (down_casted_an_arg) {
+          try {
+            Method m = o.getClass().getDeclaredMethod(method_name, arg_classes);
+            m.setAccessible(true);
+            return (T) m.invoke(o, args);
+          }
+          catch (Exception e2) {
+            System.out.println(e2);
+          }
+        }
+      }
+      else {
+        System.out.println(e);
+      }
     }
     return null;
   }
